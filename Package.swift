f47655eef9ce18c142c1fe1b6da71ce01241067a
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2023-06-30 16:28:59 +0700

    Add AmityUIKitLiveStreamSharedFrameworks

--- Package.swift
+++ Package.swift
@@ -8,19 +8,24 @@ let package = Package(
     products: [
         .library(
             name: "AmityUIKit",
-            targets: ["AmityUIKit","AmityUIKitLiveStream"]
+            targets: ["AmityUIKit","AmityUIKitLiveStream","AmityUIKitLiveStreamSharedFrameworks"]
         ),
     ],
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKit.xcframework.zip?1.0.4",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKit.xcframework.zip?1.0.5",
             checksum: "51d0351407bcea2d5a725bd3fbf64e17f9bbbf90fcfe12d9793a2e66f0116172"
         ),
         .binaryTarget(
             name: "AmityUIKitLiveStream",
-            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStream.kk.xcframework.zip",
-            checksum: "adee925d52d5a64313e4b1e7562f21a457798a73de4ae036a991988ff367f025"
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStream.kk.xcframework.zip?1.0.5",
+            checksum: "5c23aaae603def45e3d8263ddf89291aa98f7476c343edfde43bcf569767f182"
+        ),
+        .binaryTarget(
+            name: "AmityUIKitLiveStreamSharedFrameworks",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStreamSharedFrameworks.xcframework.zip?1.0.5",
+            checksum: "4214c01e938744a48f1984f5d55e7a6b312df1b2e2c1d720623ca6d89ccf05fa"
         ),
     ]
 )

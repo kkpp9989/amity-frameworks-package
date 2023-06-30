1a3d625af4b42660fb8c8d5004f9f882ba8d329b
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2023-06-30 16:31:25 +0700

    fix path AmityUIKitLiveStream

--- Package.swift
+++ Package.swift
@@ -14,17 +14,17 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AmityUIKit",
-            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKit.xcframework.zip?1.0.5",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKit.xcframework.zip?1.0.7",
             checksum: "51d0351407bcea2d5a725bd3fbf64e17f9bbbf90fcfe12d9793a2e66f0116172"
         ),
         .binaryTarget(
             name: "AmityUIKitLiveStream",
-            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStream.kk.xcframework.zip?1.0.5",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStream.xcframework.zip?1.0.7",
             checksum: "5c23aaae603def45e3d8263ddf89291aa98f7476c343edfde43bcf569767f182"
         ),
         .binaryTarget(
             name: "AmityUIKitLiveStreamSharedFrameworks",
-            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStreamSharedFrameworks.xcframework.zip?1.0.5",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStreamSharedFrameworks.xcframework.zip?1.0.7",
             checksum: "4214c01e938744a48f1984f5d55e7a6b312df1b2e2c1d720623ca6d89ccf05fa"
         ),
     ]

beb989c0c086a83609e98ba9da7232c172cbf115
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2023-06-30 13:52:43 +0700

    Update Package.swift add AmityUIKitLiveStream

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "AmityUIKit",
-            targets: ["AmityUIKit"]
+            targets: ["AmityUIKit","AmityUIKitLiveStream"]
         ),
     ],
     targets: [
@@ -16,6 +16,11 @@ let package = Package(
             name: "AmityUIKit",
             url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKit.xcframework.zip",
             checksum: "51d0351407bcea2d5a725bd3fbf64e17f9bbbf90fcfe12d9793a2e66f0116172"
+        ),
+        .binaryTarget(
+            name: "AmityUIKitLiveStream",
+            url: "https://amity-uikit-dependency.firebaseapp.com/AmityUIKitLiveStream.xcframework.zip",
+            checksum: "2cdb771b1311ee3bd499948cb49b05874575c6f6c9e997fda3dfca4bb38b56df"
         ),
     ]
 )

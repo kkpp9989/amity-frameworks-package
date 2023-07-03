5e56f0c884889121600000b0f0cdc80700a3edc4
kkpp9989 <117346116+kkpp9989@users.noreply.github.com>
2023-07-03 09:26:40 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,23 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "AmitySharedFrameworks",
+    name: "AmitySDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "AmitySharedFrameworks",
-            targets: ["AmitySharedFrameworks", "AmitySDK", "Realm", "RealmSwift", "AmityLiveVideoBroadcastKit", "AmityVideoPlayerKit", "MobileVLCKit"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "AmitySDK",
+            targets: ["AmitySDK", "Realm", "RealmSwift", "AmityLiveVideoBroadcastKit", "AmityVideoPlayerKit", "MobileVLCKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "AmitySharedFrameworks",
-            dependencies: []),
         .binaryTarget(
                     name: "AmitySDK",
                     url: "https://sdk.amity.co/sdk-release/ios-uikit-frameworks/3.5.0/AmitySDK.xcframework.zip",
